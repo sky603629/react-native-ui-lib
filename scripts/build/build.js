62a5bfc38a1209a01009917ef6d35c86033d91e2
sky603629 <sunkaiyang@h-partners.com>
2025-12-17 17:28:54 +0800

    pre-release: @react-native-ohos/react-native-ui-lib@7.43.1-rc.2
    Signed-off-by: sky603629 <sunkaiyang@h-partners.com>

--- scripts/build/build.js
+++ scripts/build/build.js
@@ -1,22 +1,22 @@
 const childProcess = require('child_process');
 const fs = require('fs');
 
-const BABEL_OPTIONS = `--config-file .\\src\\.babelrc.json --extensions .ts,.tsx --ignore "src\\**\\*.d.ts,src\\native"`;
-const BABEL_INDEX_EXPORTS_OPTIONS = `--config-file .\\src\\.babelrc.exports.js`;
+const BABEL_OPTIONS = `--config-file ./src/.babelrc.json --extensions .ts,.tsx --ignore "src/**/*.d.ts,src/native"`;
+const BABEL_INDEX_EXPORTS_OPTIONS = `--config-file ./src/.babelrc.exports.js`;
 
 console.info('## Start RNUILib Build ##');
 
 console.info('## Build Typescript ##');
-childProcess.execSync('tsc --p tsconfig.build.json', { stdio: 'inherit' });
+// childProcess.execSync('tsc --p tsconfig.build.json');
 
 console.info('## Build src files - convert TS to JS files ##');
-childProcess.execSync(`.\\node_modules\\.bin\\babel src --out-dir src ${BABEL_OPTIONS}`, { stdio: 'inherit' });
+childProcess.execSync(`npx babel src --out-dir src ${BABEL_OPTIONS}`);
 
 console.info('## Build lib (native component) files - convert TS to JS files ##');
-childProcess.execSync(`.\\node_modules\\.bin\\babel lib --out-dir lib ${BABEL_OPTIONS}`, { stdio: 'inherit' });
+childProcess.execSync(`npx babel lib --out-dir lib ${BABEL_OPTIONS}`);
 
 console.info('## Build main index file - for lazy load exports ##');
-childProcess.execSync(`.\\node_modules\\.bin\\babel .\\src\\index.js -o .\\src\\index.js ${BABEL_INDEX_EXPORTS_OPTIONS}`, { stdio: 'inherit' });
+childProcess.execSync(`npx babel ./src/index.js -o ./src/index.js ${BABEL_INDEX_EXPORTS_OPTIONS}`);
 
 console.info('## Build standalone components packages ##');
 require('./buildPackages');
